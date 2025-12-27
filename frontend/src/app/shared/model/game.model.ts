75de500b780e3f934e9220bd37d8df8432969d10
frittich <ffflllooo6@gmail.com>
2025-12-27 02:41:39 +0100

    feature(Models): Added models for datatypes which align with the database and backend

--- frontend/src/app/shared/model/game.model.ts
+++ frontend/src/app/shared/model/game.model.ts
@@ -1,21 +1,32 @@
-import {User} from './user.model';
-
+import { GameQuestion } from './question.model.ts';
 export interface Game {
-  CreatedAt: Date;
-  Owner: User;
-  GameName: string;
-  Players: User[];
-  GameQuestions: GameQuestion[];
-  Settings: GameSettings;
+  game_id: string
+  Owner_id: string;
+  created_at: Date;
+  game_name: string;
+  game_status: GameStatus
+  game_players: GamePlayers[];
+  game_settings: GameSettings;
+  game_questions: GameQuestion[];
+}
+
+export enum GameStatus {
+  PLANNING = 'Planung',
+  SCHEDULED = 'Terminierung',
+  CLOSED = 'Abgeschlossen',
+}
+
+export enum PlayerRole {
+  ADMIN = 'Administrator',
+  MODERATER = 'Moderator',
+  USER = 'Benutzer',
+}
+
+export interface GamePlayers {
+  user_id: string;
+  role: PlayerRole;
 }
 
 export interface GameSettings {
   type: string;
-}
-
-export interface GameQuestion {
-  Question: string;
-  Hint1: string;
-  Hint2: string;
-  Answer: string;
 }
